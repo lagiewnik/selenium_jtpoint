0b9ba254314bb7ee949ff36bcad23b73530f39d1
lagiewnik <lagiewnik@gmail.com>
2019-07-09 21:36:52 +0200

    Add .gitignore Add First Test Case

--- selenium_jtpoint/src/selenium_jtpoint/First.java
+++ selenium_jtpoint/src/selenium_jtpoint/First.java
@@ -1,9 +1,26 @@
 package selenium_jtpoint;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class First {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// declaration and instantiation of objects/variables
+		System.setProperty("webdriver.chrome.driver", "c:\\selenium_web_driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	//Launch website
+		driver.navigate().to("https://www.google.pl");
+		
+	//Click on the search text box and send value
+		
+		driver.findElement(By.name("q")).sendKeys("javatpoint tutorials");
+		
+	// Click on the search button
+		driver.findElement(By.name("btnK")).click();
 
 	}
 
