fbf149edcd59f51f58efcf4c85a731e53bc5833a
lagiewnik <lagiewnik@gmail.com>
2019-07-13 18:58:51 +0200

    Add drag and drop test. Comment other test

--- selenium_jtpoint/src/selenium_jtpoint/Commands.java
+++ selenium_jtpoint/src/selenium_jtpoint/Commands.java
@@ -1,8 +1,12 @@
 package selenium_jtpoint;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.Select;
 
@@ -19,6 +23,7 @@ public class Commands {
 		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setCapability("marionette", true);
 		WebDriver driver = new FirefoxDriver(capabilities);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 // LAunch Website
 		driver.navigate().to("https://www.testandquiz.com/selenium/testing.html");
@@ -33,27 +38,33 @@ public class Commands {
 		//driver.findElement(By.linkText("This is a link")).click();
 		//driver.navigate().back();
         // Click on the textbox and send value  
-		driver.findElement(By.id("fname")).sendKeys("tekstuj");
+		//driver.findElement(By.id("fname")).sendKeys("tekstuj");
 		System.out.println("Wpisany text");
 		
+	
         // Clear the text written in the textbox  
-		driver.findElement(By.id("fname")).clear();
+		//driver.findElement(By.id("fname")).clear();
 		System.out.println("Clear text");
 		
         // Click on the Submit button using click() command  
-		driver.findElement(By.id("idOfButton")).click();
-		
-        // Locate the radio button by id and check it using click() function  
-		driver.findElement(By.id("male")).click();  
+		//driver.findElement(By.id("idOfButton")).click();
+		System.out.println("Click Submit button");
         
-		
+		// Locate the radio button by id and check it using click() function  
+		//driver.findElement(By.id("male")).click();  
+		System.out.println("radio button click");
+      	
         // Locate the checkbox by cssSelector and check it using click() function  
-	     driver.findElement(By.cssSelector("input.Automation")).click();  
+	     //driver.findElement(By.cssSelector("input.Automation")).click();  
+	     System.out.println("checkbox click"); 
 		
         // Use Select class for selecting value from dropdown  
-	     Select dropdown = new Select(driver.findElement(By.id("testingDropdown")));  
-	     dropdown.selectByVisibleText("Automation Testing");  
-	        
+	     WebElement dropdown = driver.findElement(By.id("sourceImage"));  
+	     WebElement target = driver.findElement(By.id("targetDiv"));
+	     (new Actions(driver)).dragAndDrop(dropdown, target);
+	     
+	     //dropdown.selectByVisibleText("Automation Testing");  
+	     System.out.println("drop down test");    
 		
         // Close the Browser  
 		
